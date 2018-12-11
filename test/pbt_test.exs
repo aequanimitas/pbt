a5bbda4262fde86263584fb34158673042857d14
aequanimitas <htavenido@gmail.com>
2018-12-11 17:22:16 +0800

    generalizing example tests

--- test/pbt_test.exs
+++ test/pbt_test.exs
@@ -12,12 +12,21 @@ defmodule PbtTest do
 
   property "biggest" do
     forall lst <- non_empty(list(integer())) do
-      Pbt.biggest(lst) == lst |> Enum.sort() |> List.last()
+      Pbt.biggest(lst) == model_biggest(lst)
+    end
+  end
+
+  property "picks last number" do
+    forall {lst, known_last} <- {non_empty(list(number())), number()} do
+      known_list = lst ++ [known_last]
+      known_last == List.last(known_list)
     end
   end
 
   # helpers
   def boolean(_), do: true
+
+  def model_biggest(list), do: list |> Enum.sort() |> List.last()
 
   # generators
   def my_type, do: term()
